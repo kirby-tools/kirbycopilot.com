fc6b94d702ec135ffa28f4af3c7fb306e789126b
Johann Schopplich <mail@johannschopplich.com>
2024-01-23 18:36:37 +0100

    chore: remove Cf deploy pipeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
 
   nitro: {
     prerender: {
-      routes: ["/", "/docs/getting-started"],
+      routes: ["/", "/docs/getting-started/installation"],
     },
   },
 });
