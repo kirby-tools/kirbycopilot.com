73a35e9ee857c19131d4b246bad85d9f5e2e8b93
Johann Schopplich <mail@johannschopplich.com>
2024-03-04 09:10:21 +0100

    docs: add missing homepage description

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -1,32 +1,15 @@
 <script setup lang="ts">
-import { joinURL, withLeadingSlash, withTrailingSlash } from "ufo";
-import { computed, useRuntimeConfig } from "#imports";
-
-const props = defineProps<{
+defineProps<{
   src?: string;
   alt?: string;
   width?: string | number;
   height?: string | number;
 }>();
-
-const refinedSrc = computed(() => {
-  if (props.src?.startsWith("/") && !props.src.startsWith("//")) {
-    const _base = withLeadingSlash(
-      withTrailingSlash(useRuntimeConfig().app.baseURL),
-    );
-
-    if (_base !== "/" && !props.src.startsWith(_base)) {
-      return joinURL(_base, props.src);
-    }
-  }
-
-  return props.src;
-});
 </script>
 
 <template>
   <NuxtImg
-    :src="refinedSrc"
+    :src="src"
     :alt="alt || ''"
     :width="width"
     :height="height"
